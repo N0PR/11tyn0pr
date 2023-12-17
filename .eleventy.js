0b36391178d7e8af19f2b72d69adb75ef76920c2
Ira Huff <ira.m.huff@gmail.com>
2023-12-16 20:17:39 -0600

    adding javascript

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('./src/main.css');
     eleventyConfig.addPassthroughCopy('./src/img');
+    eleventyConfig.addPassthroughCopy('./src/main.js');
     
     return {
         dir: {
---
+++ src/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+        (function(d, s, id) {
+            if (d.getElementById(id)) {
+                if (window.__TOMORROW__) {
+                    window.__TOMORROW__.renderWidget();
+                }
+                return;
+            }
+            const fjs = d.getElementsByTagName(s)[0];
+            const js = d.createElement(s);
+            js.id = id;
+            js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
+
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'tomorrow-sdk');
+        
+
+       
