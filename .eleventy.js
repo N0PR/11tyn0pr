2c1e4e2656592ce9c7b66312372d6eaffe27e9c4
N0PR <n0prmacon@gmail.com>
2024-01-26 10:54:45 -0600

    changed club meetings to club newsletters added robot.txt and stitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/main.css');
     eleventyConfig.addPassthroughCopy('./src/img');
     eleventyConfig.addPassthroughCopy('./src/main.js');
+    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+    eleventyConfig.addPassthroughCopy('./src/newsletters');
     
     return {
         dir: {
