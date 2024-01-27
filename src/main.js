7a64b6c6cf0f200075985ebbecbf3ed18b24319c
N0PR <n0prmacon@gmail.com>
2024-01-27 11:53:01 -0600

    cleaned up javascript file

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
 "use strict";
 
-        (function(d, s, id) {
-            if (d.getElementById(id)) {
-                if (window.__TOMORROW__) {
-                    window.__TOMORROW__.renderWidget();
-                }
-                return;
-            }
-            const fjs = d.getElementsByTagName(s)[0];
-            const js = d.createElement(s);
-            js.id = id;
-            js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
-
-            fjs.parentNode.insertBefore(js, fjs);
-        })(document, 'script', 'tomorrow-sdk');
-        
-
 const navBar = () => {
   let x = document.getElementById("nav");
   if (x.className === "nav") {
